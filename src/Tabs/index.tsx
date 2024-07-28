1e85bb0d35b424ebc7c9bd38718699c4f504e6ab
Gabriel Castro <gabrieldcc@gmail.com>
2024-07-28 15:23:59 -0300

    add bottom tabs

--- src/Tabs/index.tsx
+++ src/Tabs/index.tsx
@@ -1,23 +1,38 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Principal from "./Principal";
 import Consultas from "./Consultas";
-
-console.log(Principal);
-console.log(Consultas);
+import { SafeAreaView } from "react-native-safe-area-context";
+import Ionicons from 'react-native-vector-icons/Ionicons'
 
 const Tab = createBottomTabNavigator()
 
 export default function Tabs() {
     return (
-        <Tab.Navigator>
-            <Tab.Screen 
-            name="Principal"
-            component={Principal}
-            />
-            <Tab.Screen 
-            name="Consultas"
-            component={Consultas}
-            />
-        </Tab.Navigator>
+        <SafeAreaView style={{ flex: 1 }}>
+            <Tab.Navigator>
+                <Tab.Screen
+                    name="Principal"
+                    component={Principal}
+                    options={{
+                        headerShown: false,
+                        tabBarIcon: () => (
+                            <Ionicons
+                                name="home"/>
+                        )
+                    }}
+                />
+                <Tab.Screen
+                    name="Consultas"
+                    component={Consultas}
+                    options={{
+                        headerShown: false,
+                        tabBarIcon: () => (
+                            <Ionicons
+                                name="calendar"/>
+                        )
+                    }}
+                />
+            </Tab.Navigator>
+        </SafeAreaView>
     )
 }
